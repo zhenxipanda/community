61041f88fbd8ee29531a157d8ec5a0132a1a36bf
zhenxipanda <1359619070@qq.com>
2020-05-15 20:40:52 +0800

    add sign in

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
  ## 论坛
  
  ## 资料
- https://spring.io/guides
- https://spring.io/guides/gs/serving-web-content/
- https://elasticaearch.cn/explore
+ [Spring 文档](https://spring.io/guides)  
+ [Spring Web文档](https://spring.io/guides/gs/serving-web-content/)  
+ [es 社区](https://elasticaearch.cn/explore)  
+ [Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+ 
  
  ## 工具
---
+++ src/main/java/life/zhaohuan/community/community/controller/AuthorizedController.java
@@ -0,0 +1,38 @@
+package life.zhaohuan.community.community.controller;
+
+import life.zhaohuan.community.community.dto.AccessTokenDTO;
+import life.zhaohuan.community.community.dto.GithubUser;
+import life.zhaohuan.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizedController {
+    @Autowired
+    public GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code ,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
