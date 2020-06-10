e526237b9e804bb0e4d7ac269166862d2083df3c
zhenxipanda <1359619070@qq.com>
2020-06-10 21:02:08 +0800

    finish some modification

--- src/main/java/life/zhaohuan/community/community/controller/AuthorizedController.java
+++ src/main/java/life/zhaohuan/community/community/controller/AuthorizedController.java
@@ -50,7 +50,7 @@ public class AuthorizedController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            user.setAvatarUrl(githubUser.getAvatar_url());
+            user.setAvatarUrl(githubUser.getAvatarUrl());
             userMapper.insert(user);
             response.addCookie(new Cookie("token" , token));
             return "redirect:/";
