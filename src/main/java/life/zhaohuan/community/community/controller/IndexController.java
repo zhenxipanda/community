6734d2b43cbf7b8882b7d830d02b7911ceef08e0
zhenxipanda <1359619070@qq.com>
2020-06-14 22:10:58 +0800

    finish fenye 1

--- src/main/java/life/zhaohuan/community/community/controller/IndexController.java
+++ src/main/java/life/zhaohuan/community/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package life.zhaohuan.community.community.controller;
 
+import life.zhaohuan.community.community.dto.PaginationDTO;
 import life.zhaohuan.community.community.dto.QuestionDTO;
 import life.zhaohuan.community.community.mapper.QuestionMapper;
 import life.zhaohuan.community.community.mapper.UserMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -25,7 +27,9 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size){
         Cookie[] cookies = request.getCookies();
         if(cookies != null && cookies.length != 0){
             for (Cookie cookie : cookies) {
@@ -42,8 +46,8 @@ public class IndexController {
 
             }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/life/zhaohuan/community/community/service/QuestionService.java
+++ src/main/java/life/zhaohuan/community/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.zhaohuan.community.community.service;
 
+import life.zhaohuan.community.community.dto.PaginationDTO;
 import life.zhaohuan.community.community.dto.QuestionDTO;
 import life.zhaohuan.community.community.mapper.QuestionMapper;
 import life.zhaohuan.community.community.mapper.UserMapper;
@@ -21,9 +22,13 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        //size*(page-1)
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset , size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        PaginationDTO paginationDTO = new PaginationDTO();
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -31,6 +36,9 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        return paginationDTO;
     }
 }
