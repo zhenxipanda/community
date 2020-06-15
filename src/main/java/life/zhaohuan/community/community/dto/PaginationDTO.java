d3093e8b8fa900f3d09f5a398cb1efd9a4de28fe
zhenxipanda <1359619070@qq.com>
2020-06-15 20:46:15 +0800

    finish fenye 2

--- src/main/java/life/zhaohuan/community/community/dto/PaginationDTO.java
+++ src/main/java/life/zhaohuan/community/community/dto/PaginationDTO.java
@@ -17,16 +17,22 @@ public class PaginationDTO {
     private boolean showEndPage;
     private  Integer page;
     private  List<Integer> pages = new ArrayList<>();
+    private  Integer totalPage;
 
     public void setPagination(Integer totalCount, Integer page, Integer size) {
-        Integer totalPage;
         if(totalCount % size == 0){
             totalPage = totalCount / size ;
         }
         else{
             totalPage = totalCount / size + 1 ;
         }
-
+        if(page < 1){
+            page = 1;
+        }
+        if(page > totalPage){
+            page = totalPage;
+        }
+        this.page = page;
         pages.add(page);
         for(int i = 1;i <= 3 ;i ++){
             if(page - i > 0){
