c132ed4dbd918eaba86627849aaba7b2bdd3903e
zhenxipanda <1359619070@qq.com>
2020-06-24 18:57:11 +0800

    finish log in interceptor

--- src/main/java/life/zhaohuan/community/community/controller/IndexController.java
+++ src/main/java/life/zhaohuan/community/community/controller/IndexController.java
@@ -1,11 +1,6 @@
 package life.zhaohuan.community.community.controller;
 
 import life.zhaohuan.community.community.dto.PaginationDTO;
-import life.zhaohuan.community.community.dto.QuestionDTO;
-import life.zhaohuan.community.community.mapper.QuestionMapper;
-import life.zhaohuan.community.community.mapper.UserMapper;
-import life.zhaohuan.community.community.model.Question;
-import life.zhaohuan.community.community.model.User;
 import life.zhaohuan.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,39 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
 @Controller
 public class IndexController {
-    @Autowired
-    private UserMapper userMapper;
+
 
     @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
+    public String index(Model model,
                         @RequestParam(name = "page",defaultValue = "1") Integer page,
                         @RequestParam(name = "size",defaultValue = "5") Integer size){
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null && cookies.length != 0){
-            for (Cookie cookie : cookies) {
-                if(cookie != null){
-                    if(cookie.getName().equals("token")){
-                        String token = cookie.getValue();
-                        User user = userMapper.findByToken(token);
-                        if(user != null){
-                            request.getSession().setAttribute("user" , user);
-                        }
-                        break;
-                    }
-                }
-
-            }
-        }
         PaginationDTO pagination = questionService.list(page,size);
         model.addAttribute("pagination",pagination);
         return "index";
--- src/main/java/life/zhaohuan/community/community/controller/ProfileController.java
+++ src/main/java/life/zhaohuan/community/community/controller/ProfileController.java
@@ -1,25 +1,21 @@
 package life.zhaohuan.community.community.controller;
 
 import life.zhaohuan.community.community.dto.PaginationDTO;
-import life.zhaohuan.community.community.mapper.UserMapper;
 import life.zhaohuan.community.community.model.User;
 import life.zhaohuan.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
 
-    @Autowired
-    private UserMapper userMapper;
+
     @Autowired
     private QuestionService questionService;
 
@@ -29,23 +25,7 @@ public class ProfileController {
                           Model model,
                           @RequestParam(name = "page",defaultValue = "1") Integer page,
                           @RequestParam(name = "size",defaultValue = "5") Integer size){
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null && cookies.length != 0){
-            for (Cookie cookie : cookies) {
-                if(cookie != null){
-                    if(cookie.getName().equals("token")){
-                        String token = cookie.getValue();
-                        user = userMapper.findByToken(token);
-                        if(user != null){
-                            request.getSession().setAttribute("user" , user);
-                        }
-                        break;
-                    }
-                }
-
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
         // 如果用户为空，就跳到登录页面
         if(user == null){
             return "redirect:/";
