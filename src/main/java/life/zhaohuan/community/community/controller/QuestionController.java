fd558e344485f63f75cbf2716a7ed3042720cf88
zhenxipanda <1359619070@qq.com>
2020-06-25 19:26:52 +0800

    finish question html

---
+++ src/main/java/life/zhaohuan/community/community/controller/QuestionController.java
@@ -0,0 +1,25 @@
+package life.zhaohuan.community.community.controller;
+
+import life.zhaohuan.community.community.dto.QuestionDTO;
+import life.zhaohuan.community.community.mapper.QuestionMapper;
+import life.zhaohuan.community.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    public String question(@PathVariable(name = "id") Integer id,
+                           Model model){
+        QuestionDTO questionDTO = questionService.getById(id);
+        model.addAttribute("question" , questionDTO);
+        return "question";
+    }
+}
