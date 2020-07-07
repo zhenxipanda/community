be7fcf5856943db778a179bd3937d7068b80d52e
zhenxipanda <1359619070@qq.com>
2020-07-07 19:18:25 +0800

    add Transactional

--- src/main/java/life/zhaohuan/community/community/service/CommentService.java
+++ src/main/java/life/zhaohuan/community/community/service/CommentService.java
@@ -10,6 +10,7 @@ import life.zhaohuan.community.community.model.Comment;
 import life.zhaohuan.community.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CommentService {
@@ -21,6 +22,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
        if(comment.getParentId() == null || comment.getParentId() == 0){
            throw new CustomizedException(CustomizedErrorCode.TARGET_PARAM_NOT_FOUND);
