9dfb246103609bdc8d1626e4889dd38433a670c8
zhenxipanda <1359619070@qq.com>
2020-07-08 19:14:46 +0800

    use jquery post

---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,32 @@
+function post(){
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
+        }),
+        success: function (response) {
+            if (response.code == 200) {
+                window.location.reload();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=2859958f9f059979ed3a&redirect_uri=" + document.location.origin + "/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
+            }
+        },
+        dataType: "json"
+    });
+    console.log(questionId);
+    console.log(content);
+}
